0d7e2170f2fd9470ae36642bd2bd14d454b19c48
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-06-03 16:19:00 +0545

    feat(ImpureFunction.js): add increment impure function

--- ImpureFunction.js
+++ ImpureFunction.js
@@ -8,6 +8,21 @@ greet(); // Output: Namaste, I'm Ashish
 name = "Yeju";
 greet(); // Output: Namaste, I'm Yeju
 
+console.log("--------------------");
+
 let evenNums = [2, 4, 6, 8, 10];
 console.log(evenNums.splice(0, 3)); // Output: [ 2, 4, 6 ]
 console.log(evenNums.splice(0, 3)); // Output: [ 8, 10 ]
+
+console.log("--------------------");
+
+let counter = 0;
+
+function increment() {
+  counter++;
+  return counter;
+}
+
+console.log(increment()); // Output: 1
+
+console.log("--------------------");
